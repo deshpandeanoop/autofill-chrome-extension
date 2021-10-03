7c1818cf99fa73722fceb4def1dae8f0460877a9
deshpandeanoop <anoop.bhardwaja@gmail.com>
2021-10-03 08:53:06 +0530

    AmazonPrime auto-login
    AmazonPrime auto-login

---
+++ autofill.js
@@ -0,0 +1,11 @@
+submit();
+
+function submit() {
+  var email = document.getElementById("ap_email");
+  var password = document.getElementById("ap_password");
+
+  email.value = '<enter mobile numbe here>';
+  password.value = 'enter Password';
+
+  document.getElementById("signInSubmit").click();
+}
