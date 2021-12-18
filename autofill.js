a0c09cba9751b8453881d336c9c615bcec72d211
deshpandeanoop <anoop.bhardwaja@gmail.com>
2021-12-18 15:20:43 +0530

    Update autofill.js
    Added dynamic behaviour

--- autofill.js
+++ autofill.js
@@ -3,9 +3,22 @@ submit();
 function submit() {
   var email = document.getElementById("ap_email");
   var password = document.getElementById("ap_password");
+  var urlParams = new URLSearchParams(window.location.search);
+  var ottPlatformUserId = urlParams.get('ottPlatformUserId');
+  var platformType = urlParams.get('platformType');
 
-  email.value = '<enter mobile numbe here>';
-  password.value = 'enter Password';
-
-  document.getElementById("signInSubmit").click();
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+	 var data = JSON.parse(this.responseText);
+	 ottUserName = data.ottServiceAccounts[0].username;
+     ottPassword = data.ottServiceAccounts[0].password;
+	 
+	 email.value = ottUserName;
+	 password.value = ottPassword;
+	 document.getElementById("signInSubmit").click();
+    }
+  };
+  xhttp.open("GET", "http://localhost:9000/ott-gateway-service/v1/ottaccounts?gatewayUserId="+ottPlatformUserId+"&fetchOnlyNames=false&platformType="+platformType, true);
+  xhttp.send(); 	
 }
