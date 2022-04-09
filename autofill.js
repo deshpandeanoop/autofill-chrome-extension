f4ef0ce91ab8343645dc501c4944c58419c60ae6
deshpandeanoop <anoop.bhardwaja@gmail.com>
2022-04-09 15:48:38 +0530

    Made autofill json work on token

--- autofill.js
+++ autofill.js
@@ -2,9 +2,9 @@ submit();
 
 function submit() {
   const urlParams = new URLSearchParams(window.location.search);
-  const ottPlatformUserId = urlParams.get('ottPlatformUserId');
+  const ottPlatformUserToken = urlParams.get('ottPlatformUserToken');
   const platformType = urlParams.get('platformType');
-  let canAutofill = false;
+  let canAutofill = "false";
 
   try {
     canAutofill = urlParams.get('canAutofill');
@@ -12,13 +12,15 @@ function submit() {
     //Eat away the exception, the value defaults to false.
   }
 
-  if(canAutofill) {
+window.alert(canAutofill);
+  if(canAutofill === "false" || canAutofill == null) {
     return;
   }
 
   window.alert("AutoFilling is kicking in!!");
 
   var xhttp = new XMLHttpRequest();
+
   xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
     	 var data = JSON.parse(this.responseText);
@@ -34,6 +36,7 @@ function submit() {
     	 document.getElementById(data.meta.signInElementId).click();
     }
   };
-  xhttp.open("GET", "http://localhost:9000/ott-gateway-service/v1/ottaccounts?gatewayUserId="+ottPlatformUserId+"&fetchOnlyNames=false&platformType="+platformType, true);
+  xhttp.open("GET", "http://localhost:8080/ott-gateway-service/v1/ottaccounts?fetchOnlyNames=false&platformType="+platformType, true);
+  xhttp.setRequestHeader("Authorization", "Bearer " + ottPlatformUserToken);
   xhttp.send();
 }
